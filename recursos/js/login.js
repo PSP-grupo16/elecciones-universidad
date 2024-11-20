dd4b15b7b299587d28e6f6d2ea7925f0be4b3904
Miguel Colmenares <me@miguelcolmenares.com>
2024-11-19 21:06:09 -0500

    Actualizar rutas de navegación y redirección en el formulario de inicio de sesión para mejorar la coherencia de enlaces

--- recursos/js/login.js
+++ recursos/js/login.js
@@ -2,5 +2,5 @@ const formulario = document.querySelector('.formulario');
 
 formulario.addEventListener('submit', (e) => {
   e.preventDefault();
-  window.location.assign("/votacion");
+  window.location.assign("votacion");
 });
